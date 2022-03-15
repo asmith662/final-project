c0ae070907e75fd7d814fe0d44ce72a3c7e618d9
AndrewSmith <a.smith_662@yahoo.com>
2022-03-14 20:19:14 -0400

    current w previous

---
+++ src/main/java/com/cit_562/group_1/final_project/Entities/User.java
@@ -0,0 +1,68 @@
+package com.cit_562.group_1.final_project.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String ccBrand;
+    private String ccNum;
+    private String ccExpiration;
+    private String ccSecCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCCBrand() {
+        return ccBrand;
+    }
+
+    public void setCCBrand(String ccBrand) {
+        this.ccBrand = ccBrand;
+    }
+
+    public String getCCNum() {
+        return ccNum;
+    }
+
+    public void setCCNum(String ccNum) {
+        this.ccNum = ccNum;
+    }
+
+    public String getCCExpiration() {
+        return ccExpiration;
+    }
+
+    public void setCCExpiration(String ccExpiration) {
+        this.ccExpiration = ccExpiration;
+    }
+
+    public String getCCSecCode() {
+        return ccSecCode;
+    }
+
+    public void setCCSecCode(String ccSecCode) {
+        this.ccSecCode = ccSecCode;
+    }
+    
+}
---
+++ src/main/java/com/cit_562/group_1/final_project/Repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.cit_562.group_1.final_project.Repositories;
+
+
+
+
+import com.cit_562.group_1.final_project.Entities.User;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
